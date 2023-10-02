5a8db6447c3f68ea15350d702bd62d05a7ab0228
Rhiannon Moore <rhiannonmoore10@yahoo.com>
2023-10-02 16:29:20 -0700

    update parkcard.js destructuring

--- client/src/components/ParkCard.js
+++ client/src/components/ParkCard.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import '../components/ParkCard.css'
 
-function ParkCard({ park, reviews }) {
-  const { name, location, description } = park;
-  reviews = park.reviews
+function ParkCard({ park }) {
+  const { name, location, description, reviews } = park;
   
-    
   return (
     <div className='parkcard'>
       <div className='card-heading'>
         <h3>Name: {name}</h3>
-
       </div>
         {/* <img className="parkCardImage" src={image} alt='park'/> */}
         <div className='loction-region'>
@@ -20,8 +17,13 @@ function ParkCard({ park, reviews }) {
         <p id='descriptionP'>Description: {description}</p>
 
         <div className="reviews">
-        <h4>Reviews:</h4>
-   
+          <h4>Reviews:</h4>
+          <ul>
+            {reviews.map((review) => (
+              <li key={review.id}>{review.title}</li>
+            ))}
+          </ul>
+
       </div>
     </div>
   )
