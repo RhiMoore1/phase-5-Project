5e2c4ad31d598c270691e957d5fc869b953b9775
Rhiannon Moore <rhiannonmoore10@yahoo.com>
2023-10-02 11:50:07 -0700

    add navbar to app file

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,10 +2,12 @@ import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import ParkContainer from "./ParkContainer"
+import NavBar from "./NavBar";
 
 function App() {
   return (
     <div>
+      <NavBar />
       <Router>
         <Switch>
           <Route path="/parks">
