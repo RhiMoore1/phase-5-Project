eaf90b9f149d35b68d12e6e73cc2d7a20172fbf4
Rhiannon Moore <rhiannonmoore10@yahoo.com>
2023-10-02 11:49:49 -0700

    create navbar

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import '../components/NavBar.css';
+
+function NavBar() {
+ 
+  return (
+    <div className='navbarContainer'>
+      <h2 className='header'>Parks in Arizona</h2>
+      <nav className='navbar'>
+        <li className='navLi'>
+          <a href='/parks'>Parks</a>
+        </li>
+        <li className='navLi'>
+          <a href='/signup'>Sign Up</a>
+        </li>
+        <li className='navLi'>
+          <a href='/login'>Login</a>
+        </li>
+        <li className='navLi'>
+          <a href="/reviews">Reviews</a>
+        </li>
+      </nav>
+    </div>
+  );
+}
+
+export default NavBar;
+
+
