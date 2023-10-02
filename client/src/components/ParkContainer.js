950011d6633db09238d27f0df27c80ed3442a451
Rhiannon Moore <rhiannonmoore10@yahoo.com>
2023-10-02 16:29:49 -0700

    update parkContainer destructuring and endpoint

--- client/src/components/ParkContainer.js
+++ client/src/components/ParkContainer.js
@@ -5,24 +5,36 @@ import { NavLink } from "react-router-dom";
 
 function ParkContainer() {
 
-    const [parks, setParks] = useState([]);
+    // const [parks, setParks] = useState([]);
+    const [parksWithReviews, setParksWithReviews] = useState([]);
    
      useEffect(() => {
-        fetch('/parks')
-            .then(r => r.json())
-            .then((data) => setParks(data))
-            .catch((error) => console.log('Error fetching parks', error));
+        // fetch('/parks')
+        //     .then(r => r.json())
+        //     .then((data) => setParks(data))
+        //     .catch((error) => console.log('Error fetching parks', error));
+
+        fetch('/parks_with_reviews') 
+            .then((response) => response.json())
+            .then((data) => setParksWithReviews(data))
+            .catch((error) => console.log('Error fetching parks with reviews', error));
+
+        // fetch('/reviews')
+        //     .then(r => r.json())
+        //     .then((data) => setReviews(data))
+        //     .catch((error) => console.log('Error fetching reviews', error));
+        
      }, []);
     
     return (   
         <div className='parks'>
             <div className='parks-list'>
-                {parks.map((park) => {
+                {parksWithReviews.map((park) => {
                     return (
                         <ParkCard 
                             key={park.id} 
                             park={park}      
-                                          
+                                   
                         />
                     )
                 })}
