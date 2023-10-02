db710dad1cad7ba235663c3f6f3ca63631050ea0
Rhiannon Moore <rhiannonmoore10@yahoo.com>
2023-10-02 11:20:01 -0700

    create empty revision

---
+++ server/migrations/versions/0fff83b51aa4_create_db.py
@@ -0,0 +1,24 @@
+"""Create DB
+
+Revision ID: 0fff83b51aa4
+Revises: 
+Create Date: 2023-10-02 11:10:41.063282
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '0fff83b51aa4'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
