996e2c1ec6279677c2fff87daa83f93c198210b1
Rhiannon Moore <rhiannonmoore10@yahoo.com>
2023-10-02 16:30:13 -0700

    update reviewcard.js destructuring

--- client/src/components/ReviewCard.js
+++ client/src/components/ReviewCard.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import '../components/ReviewCard.css'
 
-function ReviewCard({ review, park }) {
+function ReviewCard({ review }) {
 
-  const { title } = review;
+  const { title, park_id, user_id } = review;
+ 
   
   return (
     <div className='reviewCard'>
       <div className='card-heading'>
         <h3>Review: {title}</h3>
+        <h4>Park Id: {park_id}</h4>
+        <h4>User Id: {user_id}</h4>
         
       </div>
     </div>
