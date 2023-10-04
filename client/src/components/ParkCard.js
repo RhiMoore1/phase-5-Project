6415e7bc3fcbb7543123fc9f04931e85d1e34bd1
Rhiannon Moore <rhiannonmoore10@yahoo.com>
2023-10-04 08:30:54 -0700

    add username to review

--- client/src/components/ParkCard.js
+++ client/src/components/ParkCard.js
@@ -2,25 +2,25 @@ import React from 'react';
 import '../components/ParkCard.css'
 
 function ParkCard({ park }) {
-  const { name, image, location, description, reviews } = park;
+  const { name, image, location, description, reviews, user_name } = park;
   
   return (
     <div className='parkcard'>
       <div className='card-heading'>
-        <h3>{name}</h3>
+        <h3 id='parkName'>{name}</h3>
       </div>
         <img src={image} className="parkCardImage" alt='park'/>
-        <div className='loction-region'>
-          <p>{location}</p>
+        <div className='location-region'>
+          <p id='location'>{location}</p>
           
         </div>
         <p id='descriptionP'>{description}</p>
 
         <div className="reviews">
           <h4>Park Reviews:</h4>
-          <ul>
+          <ul className='reviewList'>
             {reviews.map((review) => (
-              <li key={review.id}>{review.title}</li>
+              <li key={review.id}>&ldquo;{review.title}&rdquo; - <span id="reviewerName">{review.user_name}</span></li>
             ))}
           </ul>
 
