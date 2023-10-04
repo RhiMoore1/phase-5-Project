8b4b20c07c7b57e191a9b5dce18f9523b522df5b
Rhiannon Moore <rhiannonmoore10@yahoo.com>
2023-10-03 17:16:00 -0700

    update park card titles

--- client/src/components/ParkCard.js
+++ client/src/components/ParkCard.js
@@ -2,22 +2,22 @@ import React from 'react';
 import '../components/ParkCard.css'
 
 function ParkCard({ park }) {
-  const { name, location, description, reviews } = park;
+  const { name, image, location, description, reviews } = park;
   
   return (
     <div className='parkcard'>
       <div className='card-heading'>
-        <h3>Name: {name}</h3>
+        <h3>{name}</h3>
       </div>
-        {/* <img className="parkCardImage" src={image} alt='park'/> */}
+        <img src={image} className="parkCardImage" alt='park'/>
         <div className='loction-region'>
-          <p>Location: {location}</p>
+          <p>{location}</p>
           
         </div>
-        <p id='descriptionP'>Description: {description}</p>
+        <p id='descriptionP'>{description}</p>
 
         <div className="reviews">
-          <h4>Reviews:</h4>
+          <h4>Park Reviews:</h4>
           <ul>
             {reviews.map((review) => (
               <li key={review.id}>{review.title}</li>
